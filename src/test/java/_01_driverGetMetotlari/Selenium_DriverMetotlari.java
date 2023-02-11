914ec28e21b68ef4e093a6fe4b2d08d19333f9a8
erhan paker <erhanpaker@gmail.com>
2023-02-11 13:38:00 +0300

    Driver Get Metotlari

--- src/test/java/_01_driverGetMetotlari/Selenium_DriverMetotlari.java
+++ src/test/java/_01_driverGetMetotlari/Selenium_DriverMetotlari.java
@@ -9,11 +9,14 @@ public class Selenium_DriverMetotlari extends TestBaseMetod {
 
     public void driverGetMetot(){
 
-        // 1- Driver istenen url götür
+        // 1- Driver tarayıcıyı istenen url götürür
         driver.get("https://www.amazon.com/");
 
-        // 2-  Gidilen Web sayfasının URL bilgisini döndürür
-        driver.getCurrentUrl();
+        // 2-  Gidilen Web sayfasının URL bilgisini döndürür - yazdırır
+        System.out.println(driver.getCurrentUrl());
+
+        // Gidilen Web sayfasinin title bilgisini döndür - yazdırır
+        System.out.println(driver.getTitle());
 
 
     }
