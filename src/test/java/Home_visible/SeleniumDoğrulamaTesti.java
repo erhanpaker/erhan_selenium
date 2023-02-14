e0e5064dac1fc5f7d6babc1dbcc306cef3c7c940
erhan paker <erhanpaker@gmail.com>
2023-02-14 17:25:22 +0300

    Selenium Doğrulama Assertions

--- src/test/java/Home_visible/SeleniumDoğrulamaTesti.java
+++ src/test/java/Home_visible/SeleniumDoğrulamaTesti.java
@@ -13,11 +13,27 @@ public class SeleniumDoğrulamaTesti extends TestBaseMetod {
 
     public void dogrulama(){
 
+        // Junit ile "assertion için en çok kullanılan 4 metod
+
+        // 1- sonuç 20 olduğunu test et;
+        // 1  Assert.assertEquals( sonuc , 20);
+
+        // 2 - Sonucun Succesfull olmadığını test eden kod
+        // 2 - Assert.assertNotEquals(sonuc , succesfull);
+
+        // 3- Sayının 50 büyül olduğunu test eden kod
+        // 3-  Assert.assertTrue(sayi> 50);
+
+        // 4- Sayinın 50 den büyük olmadığını test edin
+        // 4-    Assert.assertFalse(sayi>50);
+
+
+
         //  1 Sayfa başlığının “YouTube” oldugunu test edin - iki farklı senaryo
 
         driver.get("https://www.youtube.com");
 
-        String expectedTitle="youtube.com";
+        String expectedTitle="https://www.youtube.com";
 
         String actualTitle= driver.getTitle();
         Assert.assertEquals(expectedTitle,actualTitle);
@@ -35,6 +51,13 @@ public class SeleniumDoğrulamaTesti extends TestBaseMetod {
 
         String  actualUrl = driver.getCurrentUrl();
         Assert.assertTrue(actualUrl.contains(expectedTitle));
+
+
+
+
+
+
+
 
     }
 
